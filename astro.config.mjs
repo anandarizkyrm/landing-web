af1d19200dc7e4fa0cca29999036659789cf8358
Ananda Rizky Ramadhan <anandarizkyrm@gmail.com>
2022-10-23 10:31:25 +0700

    set deployment

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,4 +1,7 @@
-import { defineConfig } from 'astro/config';
+import { defineConfig } from "astro/config";
 
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  site: "https://anandarizkyrm.github.io",
+  base: "/landing-web",
+});
